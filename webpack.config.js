83802fb08c42449f5695b2106ba3933a6e26ed2d
Varun Arora <varun.arora@vanderbilt.edu>
2016-10-08 21:42:35 -0400

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query: {
+        presets: ['react']
+      }
     }]
   },
   externals: {
