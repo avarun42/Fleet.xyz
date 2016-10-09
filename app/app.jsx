c568e87a9ff368c49e66661e33f7ddbc71984e4c
Varun Arora <varun.arora@vanderbilt.edu>
2016-10-09 03:42:35 -0400

    live location sending and receiving

--- app/app.jsx
+++ app/app.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
+import _ from 'lodash';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.curLocation = null;
+    this.VIN = gm.info.getVIN();
+    this.state = {
+      curPosition: {
+        latitude: null,
+        longitude: null,
+        heading: null,
+        altitude: null,
+      },
+      otherLocations: {},
+    };
   }
 
   componentWillMount() {
-    gm.info.getCurrentPosition(position => {
-      this.curLocation = position;
-    }, true);
+    const positionChanged = position => {
+      this.setState({ curPosition: position.coords });
+
+      const locationToEmit = Object.assign({}, position, { VIN: this.VIN });
+      this.props.socket.emit('location', locationToEmit);
+    };
+
+    this.props.socket.on('location pack', msg => {
+      this.setState({ otherLocations: _.omit(msg, this.VIN) });
+    });
+
+    gm.info.getCurrentPosition(positionChanged, true);
+    gm.info.watchPosition(positionChanged, true);
   }
 
   render() {
+    const curPosition = `(${this.state.curPosition.latitude}, ${this.state.curPosition.longitude})`;
+
     return (
       <div>
-        <h1>{this.props.msg}. Your VIN is: {gm.info.getVIN()}</h1>
-        <p>Your current location is: {this.curLocation}</p>
+        <h1>{this.props.msg}. Your VIN is: {this.VIN}</h1>
+        <p>Your current location is: {curPosition}</p>
       </div>
     );
   }
@@ -24,6 +46,7 @@ class App extends React.Component {
 
 App.propTypes = {
   msg: React.PropTypes.string,
+  socket: React.PropTypes.instanceOf(io.Socket),
 };
 
 module.exports = App;
--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,9 @@ import ReactDOM from 'react-dom';
 
 const App = require('./app');
 
+const socket = io.connect('https://fleetxyz.azurewebsites.net', { reconnect: true });
+
 ReactDOM.render(
-  <App msg="Hello MHacks" />,
+  <App msg="Hello MHacks" socket={socket} />,
   document.getElementById('main')
 );
